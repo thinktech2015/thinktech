865a5d1e72216b93a3b2952074c49c4d616156f4
leelaecospa <leelaspa2015@gmail.com>
2015-04-14 14:53:21 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thinktech::Application.config.secret_key_base = '2480ac8f1adc59b592c481465d71117206f0f2c08c9c62590354e6318d6054ba2023da6b2291c08afd1d2dad116c179dc6314ca6b136b3067cad4309eb9c5501'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Thinktech::Application.config.session_store :cookie_store, key: '_thinktech_session'
