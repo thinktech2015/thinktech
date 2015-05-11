136819496dd5cce24f512dc14df587bd133c09e4
thinktech <thinktech2015@gmail.com>
2015-05-11 11:52:02 +0530

    ckeditor precompile added to application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module Thinktech
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+      config.assets.precompile += Ckeditor.assets
+      config.assets.precompile += %w( ckeditor/* )
+      config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+      
   end
 end
